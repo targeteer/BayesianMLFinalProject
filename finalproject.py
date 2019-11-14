1468583d0fdc19fc8c2e910684e29624db9e9656
targeteer <mysoon3765@gmail.com>
2019-11-13 18:56:14 -0600

    lda svm code fixed

--- finalproject.py
+++ finalproject.py
@@ -40,7 +40,7 @@ y = branin(xx)
 df = pd.DataFrame.from_dict(np.array([x1,x2,y]).T)
 df.columns = ['x1', 'x2', 'y']
 data_pivoted = df.pivot("x1", "x2", "y")
-ax = sns.heatmap(data_pivoted)
+# ax = sns.heatmap(data_pivoted)
 # plt.show()
 
 # (2) Describe the behavior of the function. Does it appear stationary? (That is, does the behavior
@@ -49,24 +49,45 @@ ax = sns.heatmap(data_pivoted)
 # DK : don't run this if your computer memory is filled up. y has 1,000,000(1000*1000) points and it freezes up your computer!...PS I could never run this on my computer btw..
 # stat_check = statsmodels.tsa.stattools.adfuller(df.y)
 # print(stat_check[0])
+# Data is not stationary because it does not have a constant trend, but rather has uphils and downhils.
 
+
+# (3) Can you find a transformation of the data that makes it more stationary?
+
+
+
+# (4) Make a kernel density estimate of the distribution of the values for the lda and svm benchmarks.
 # kernel density estimate of LDA
 lda = pd.read_csv("lda.csv")
 dy = lda.iloc[:,3]
-y = list(range(len(dy)))
-y = pd.Series(y)
+# y = list(range(len(dy)))
+# y = pd.Series(y)
 
-# ax = y.plot.kde()
+plt.figure(1)
+plt.title('Kernel Density Estimate of LDA')
+plt.xlabel('y')
+plt.ylabel('Density')
+# ax = dy.plot.kde()
+
+plt.figure(2)
+log_dy = np.log(dy)
+plt.title('Kernel Density Estimate of log LDA')
+plt.xlabel('log y')
+plt.ylabel('Density')
+# ax = log_dy.plot.kde()
 # plt.show()
 
 # kernel density estimate of SVM
 svm = pd.read_csv("svm.csv")
 dy = svm.iloc[:,3]
-y = list(range(len(dy)))
-y = pd.Series(y)
 
-# ax = y.plot.kde()
+# ax = dy.plot.kde()
+plt.title('Kernel Density Estimate of SVM')
+plt.xlabel('y')
+plt.ylabel('Density')
 # plt.show()
+
+# (5) Again, can you nd a transformation that makes the performance better behaved?
 
 
 # ###################
@@ -75,7 +96,7 @@ y = pd.Series(y)
 import sobol_seq
 import GPy
 
-#####Select 32 training points using sobol sequence#####
+# (1) Select a set of 32 training points for the Branin function in the domain X = [−5, 10] × [0, 15] using a Sobol sequence
 sobel_numbers = sobol_seq.i4_sobol_generate(2, 32)
 
 xx1 = sobel_numbers[:,0]
@@ -83,11 +104,16 @@ xx2 = sobel_numbers[:,1]
 xx1_domain = xx1 * 15 - 5
 xx2_domain = xx2 * 15
 
+print(xx1)
+
 
 # xx: 2-dimensional input, y : 1-d output
 xx = np.column_stack((xx1,xx2))
 input = [xx1,xx2]
 y = branin(input)
+
+
+# (2) Fit a Gaussian process model to the data using a constant mean and a squared exponential covariance
 
 # add noise to y
 noise = np.random.normal(0,0.001,len(y))
@@ -111,7 +137,7 @@ se_kernel = GPy.kern.RBF(input_dim = 2, variance = 1, lengthscale = 1.)
 m = GPy.models.GPRegression(xx, y, kernel = se_kernel)
 
 m.plot()
-# plt.show()
+plt.show()
 # print(m)
 
 #optimize
